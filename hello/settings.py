1fd728033887ea5f6b6e0083c13d4df8deddff18
XEGENDvy <114361635+XEGENDvy@users.noreply.github.com>
2023-07-07 21:36:08 +0530

    Update settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -27,6 +27,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
+#added manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER ='spadeftw2@gmail.com'
+EMAIL_HOST_PASSWORD ='zrsuaynsgufiidpt'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+
 
 # Application definition
 
